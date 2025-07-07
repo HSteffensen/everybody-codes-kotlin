d1f4f56a8e41871daae0482077e65793ab3e2758
Henry Steffensen <hsteffensen@bol.com>
2025-07-07 15:30:28 +0200

    solve day 5 part 2

--- src/main/kotlin/y2024/Day5.kt
+++ src/main/kotlin/y2024/Day5.kt
@@ -13,11 +13,13 @@ fun main() {
 3 4 5 2
 4 5 2 3
 5 2 3 4""" to 2323,
+            """2 3 4 5
+6 7 8 9""" to 6254,
         ),
         PuzzlePart(
-            Day5::clapDance,
+            Day5::clapDanceUntilRepeat,
             """2 3 4 5
-6 7 8 9""" to 50877075,
+6 7 8 9""" to 50877075L,
         ),
     )
 }
@@ -38,8 +40,10 @@ object Day5 {
         for (step in 0..<10) {
             clapDanceStep(lines, step)
         }
-        return lines.map { it.first() }.joinToString("").toInt()
+        return shoutNumber(lines)
     }
+
+    private fun shoutNumber(lines: List<List<Int>>): Int = lines.map { it.first() }.joinToString("").toInt()
 
     fun clapDanceStep(
         lines: MutableList<MutableList<Int>>,
@@ -49,8 +53,30 @@ object Day5 {
         val activeLine = (firstLine + 1) % 4
         val clapper = lines[firstLine].removeFirst()
         val activeLineLength = lines[activeLine].size
+        val clapperFinalLoop = clapper % (activeLineLength * 2)
+        if (clapperFinalLoop == 0) {
+            lines[activeLine].add(1, clapper)
+            return
+        }
         val newPosition =
-            if (clapper <= activeLineLength) clapper - 1 else activeLineLength - (clapper - activeLineLength - 1)
+            if (clapperFinalLoop <= activeLineLength) clapperFinalLoop - 1 else activeLineLength - (clapperFinalLoop - activeLineLength - 1)
         lines[activeLine].add(newPosition, clapper)
+    }
+
+    fun clapDanceUntilRepeat(input: String): Long {
+        val lines = readLines(input)
+        var step = 0
+        val shoutCounts = mutableMapOf<Int, Int>()
+        while (true) {
+            clapDanceStep(lines, step)
+            val shout = shoutNumber(lines)
+            shoutCounts.merge(shout, 1, Int::plus)
+            if (shoutCounts[shout] == 2024) {
+                break
+            }
+            step++
+        }
+
+        return shoutNumber(lines).toLong() * (step + 1).toLong()
     }
 }
